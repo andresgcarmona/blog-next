0ac2d58f5ec4626e00f2355c9e69fee09a16fe2c
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-12-31 00:07:27 -0600

    Added layout, container, footer components. Added post slug page.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 import { getAllPosts } from '../lib/api'
 import highlight from 'highlight.js'
 import php from 'highlight.js/lib/languages/php'
 import 'highlight.js/styles/dracula.css'
 import { useEffect } from 'react'
 import UserHeader from '../components/user-header'
+import Layout from '../components/layout'
+import Container from '../components/container'
 
 highlight.registerLanguage('php', php)
 
@@ -18,37 +19,30 @@ export default function Index({ posts }) {
 	const morePosts = posts.slice(1)
 	
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Andres Carmona</title>
-				<link rel="icon" href="/favicon.ico"/>
-			</Head>
-			
-			<main className={styles.main}>
-				<UserHeader />
+		<>
+			<Layout>
+				<Head>
+					<title>Andres Carmona</title>
+					<link rel="icon" href="/favicon.ico"/>
+				</Head>
 				
-				<section className="posts">
-					{posts.map(post => (
-						<div key={post.slug} className="container max-w-2xl px-3 md:px-0 pt-10 mt-10 border-t border-gray-200">
-							<div className="text-3xl font-semibold mb-4">{post.title}</div>
-							{/* <div className="content" dangerouslySetInnerHTML={{__html: post.content }}></div> */}
-							<div>{post.excerpt}</div>
-						</div>
-					))}
-				</section>
-			</main>
-			
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
-				</a>
-			</footer>
-		</div>
+				<Container>
+					<UserHeader />
+					
+					<section className="posts">
+						{posts.map(post => (
+							<div key={post.slug} className="container max-w-2xl px-3 md:px-0 pt-10 mt-10 border-t border-gray-200">
+								<div className="mb-4">
+									<a href={post.slug} className="text-3xl font-semibold">{post.title}</a>
+								</div>
+								{/* <div className="content" dangerouslySetInnerHTML={{__html: post.content }}></div> */}
+								<div>{post.excerpt}</div>
+							</div>
+						))}
+					</section>
+				</Container>
+			</Layout>
+		</>
 	)
 }
 
